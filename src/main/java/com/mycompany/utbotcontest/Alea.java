f645f830b017944baeecbbe7fbd30e97498b1e61
Hugo <mathieuxhugo@gmail.com>
2017-04-25 14:24:28 +0200

    Update Alea.java

--- src/main/java/com/mycompany/utbotcontest/Alea.java
+++ src/main/java/com/mycompany/utbotcontest/Alea.java
@@ -7,17 +7,44 @@ import java.util.Random;
  * @author HUGO
  */
 public class Alea {
-    static Random rand=new Random();
-    public static boolean pourcent(double f){
+    Random rand;
+    
+    public Alea(){
+        rand=new Random();
+    }
+    public boolean pourcentage(double f){
         if(rand.nextInt(100)<f)
             return true;
         else
             return false;
     }
-    public static boolean chancsur(int n){
+    public boolean chancsur(int n){
         if(rand.nextInt(n)<1)
             return true;
         else
             return false;
     }
+    
+    
+    public int indiceAleatoire(double tab[]){
+        int i=0;
+        double r=Math.random();
+        double somme=0.0;
+        while(r>=somme){
+            somme=tab[i]+somme;
+            i++;
+        }
+        return (i-1);
+    }
+    
+    public void testIndiceAleatoire(){
+        double tab[]={0.10,0.75,0.15};
+        int tabResult[]={0,0,0};
+        for(int i =0; i<10000; i++){
+            tabResult[indiceAleatoire(tab)]++;
+        }
+        System.out.println("Resultat attendu/resultat obtenu : 1000/"+tabResult[0]+"   7500/"+tabResult[1]+"   1500/"+tabResult[2]);
+    }
+    
 }
+
